9d24ad35fb1c0e57b39d51b88e5a4fef4e34e1e4
mauriciodelrio <mauricio.delrioz@alumnos.usm.cl>
2017-10-16 03:06:06 -0300

    test

--- lendme/models/database.js
+++ lendme/models/database.js
@@ -1,8 +1,30 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://mauricio:root@localhost:5432/lendme';
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:root@localhost:5432/lendme';
 
 const client = new pg.Client(connectionString);
 client.connect();
-const query = client.query(
-  'CREATE TABLE example(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
+const query = client.query('SELECT * FROM public.User as U');
+const queryIns = client.query('SELECT * FROM public."Institution" as I');
+const join = client.query('SELECT * FROM public."User" INNER JOIN public."Institution" ON (public."User".ins_id = public."Institution".ins_id)');
+
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on('end', (result) => { 
+  console.log(JSON.stringify(result.rows, null, "    "));
+});
+
+queryIns.on("row", function (row, result) {
+  result.addRow(row);
+});
+queryIns.on('end', (result) => { 
+console.log(JSON.stringify(result.rows, null, "    ")); 
+});
+
+join.on("row", function (row, result) {
+  result.addRow(row);
+});
+join.on('end', (result) => { 
+console.log(JSON.stringify(result.rows, null, "    "));
+client.end(); 
+});
